064c750cb3a611e2afde6896b742ec250de3107a
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2020-09-22 18:40:45 +0530

    Remove the console info as now CSS is not added from CDN.

--- src/ui/renderLaTeXAsHTML.js
+++ src/ui/renderLaTeXAsHTML.js
@@ -21,9 +21,9 @@ const CSS_FONT = 'KaTeX_Main';
 (async function() {
   const fontSupported = await canUseCSSFont(CSS_FONT);
   if (!fontSupported) {
-    console.info('Add CSS from ', CSS_CDN_URL);
     // [FS] IRAD-1061 2020-09-19
     // Now loaded locally, so that it work in closed network as well.
+    // console.info('Add CSS from ', CSS_CDN_URL);
     //injectStyleSheet(CSS_CDN_URL);
   }
 })();
