f265ebc24e52f612769ce9bdc18949ac0f11ac9e
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2020-10-19 19:08:41 +0530

    Set unique IDs for missing block (interested) nodes including document.

--- src/objectIdPlugin.js
+++ src/objectIdPlugin.js
@@ -1,63 +1,82 @@
-// [FS][16-10-2020]
-// Plugin to handle objectId generation
-
-import { Plugin, PluginKey } from "prosemirror-state"
+// [FS] IRAD-??? 2020-10-19
+// Plugin to handle automatic assign unique id to the block nodes.
+import {
+	Plugin,
+	PluginKey
+} from "prosemirror-state"
 import uuid from './uuid';
+import SetDocAttrStep from './SetDocAttrStep';
 
 const isNodeHasAttribute = (node, attrName) => {
-  Boolean(node.attrs && node.attrs[attrName]);
+	return (node.attrs && node.attrs[attrName]);
 }
 
-const attrName = "objectId";
+const isTargetNodeAllowed = (node) => {
+	return ALLOWED_NODES.includes(node.type.name);
+}
 
-export default class objectIdPlugin extends Plugin {
+const ATTR_NAME = "objectId";
+const DOC_NAME = "doc";
 
-  constructor() {
+const ALLOWED_NODES = [DOC_NAME, "paragraph", "bullet_list", "heading", "image", "list_item", "ordered_list", "table"];
 
-    super({
-      key: new PluginKey('objectId'),
-      state: {
-        init(config, state) {
-        },
-        apply(tr, set) {
-        }
-      },
-      props: {
-        handleDOMEvents: {
-          keydown(view, event) {
-            const charCode = event.key;
-            console.log(charCode);
+const requiredAddAttr = (node) => {
+	return isTargetNodeAllowed(node) && !isNodeHasAttribute(node, ATTR_NAME);
+}
 
-          }
-        },
-        nodeViews: []
-      },
-      appendTransaction: (transactions, prevState, nextState) => {
-        const tr = nextState.tr;
-        let modified = false;
-        if (transactions.some((transaction) => transaction.docChanged)) {
-          // Adds a unique id to a node
-          nextState.doc.descendants((node, pos) => {
-            const { paragraph } = nextState.schema.nodes;
-            // now the key is generated for paragraphs only 
-            //need to handle list and tables
-            if ('paragraph' === node.type.name) {
-              if (!isNodeHasAttribute(node, attrName)) {
-                const attrs = node.attrs;
-                tr.setNodeMarkup(pos, undefined, { ...attrs, [attrName]: guidGenerator() });
-                modified = true;
-              }
-            }
-          });
-        }
+export default class ObjectIdPlugin extends Plugin {
 
-        return modified ? tr : null;
-      },
-    });
-  }
+	constructor() {
+
+		super({
+			key: new PluginKey('ObjectIdPlugin'),
+			state: {
+				init(config, state) {},
+				apply(tr, set) {}
+			},
+			props: {
+				handleDOMEvents: {
+					keydown(view, event) {
+						const charCode = event.key;
+						console.log(charCode);
+
+					}
+				},
+				nodeViews: []
+			},
+			appendTransaction: (transactions, prevState, nextState) => {
+				let tr = nextState.tr;
+				let modified = false;
+				if (transactions.some((transaction) => transaction.docChanged)) {
+					// Adds a unique id to the document
+					if (requiredAddAttr(nextState.doc)) {
+						tr = tr.step(new SetDocAttrStep(ATTR_NAME, guidGenerator()));
+						modified = true;
+					}
+					
+					// Adds a unique id to a node
+					nextState.doc.descendants((node, pos) => {
+						if (requiredAddAttr(node)) {
+							if (DOC_NAME == node.type.name) {
+								tr = tr.step(new SetDocAttrStep(ATTR_NAME, guidGenerator()));
+							} else {
+								const attrs = node.attrs;
+								tr.setNodeMarkup(pos, undefined, { ...attrs,
+									[ATTR_NAME]: guidGenerator()
+								});
+								modified = true;
+							}
+						}
+					});
+				}
+
+				return modified ? tr : null;
+			},
+		});
+	}
 
 }
 // returns new guid
 function guidGenerator() {
-  return uuid();
+	return uuid();
 }
