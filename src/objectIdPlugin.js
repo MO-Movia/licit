eeb77647064d4e677c7b89e9d4d4b33712be0401
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2020-10-20 14:16:21 +0530

    Prefixed "new:". Code refactored & optimized.

--- src/objectIdPlugin.js
+++ src/objectIdPlugin.js
@@ -8,11 +8,11 @@ import uuid from './uuid';
 import SetDocAttrStep from './SetDocAttrStep';
 
 const isNodeHasAttribute = (node, attrName) => {
-  return (node.attrs && node.attrs[attrName]);
+	return (node.attrs && node.attrs[attrName]);
 }
 
 const isTargetNodeAllowed = (node) => {
-  return ALLOWED_NODES.includes(node.type.name);
+	return ALLOWED_NODES.includes(node.type.name);
 }
 
 const ATTR_NAME = "objectId";
@@ -21,96 +21,106 @@ const DOC_NAME = "doc";
 const ALLOWED_NODES = [DOC_NAME, "paragraph", "bullet_list", "heading", "image", "list_item", "ordered_list", "table"];
 
 const requiredAddAttr = (node) => {
-  return isTargetNodeAllowed(node) && !isNodeHasAttribute(node, ATTR_NAME);
+	return isTargetNodeAllowed(node) && !isNodeHasAttribute(node, ATTR_NAME);
 }
 
 export default class ObjectIdPlugin extends Plugin {
 
-  constructor() {
+	constructor() {
 
-    super({
-      key: new PluginKey('ObjectIdPlugin'),
-      state: {
-        init(config, state) { },
-        apply(tr, set) { }
-      },
-      props: {
-        handleDOMEvents: {
-          keydown(view, event) {
-            const charCode = event.key;
-            console.log(charCode);
-
-          }
-        },
-        nodeViews: []
-      },
-      appendTransaction: (transactions, prevState, nextState) => {
-        let tr = nextState.tr;
-        let modified = false;
-        if (transactions.some((transaction) => transaction.docChanged)) {
-          // Adds a unique id to the document
-          if (requiredAddAttr(nextState.doc)) {
-            tr = tr.step(new SetDocAttrStep(ATTR_NAME, guidGenerator()));
-            modified = true;
-          }
-
-          // Adds a unique id to a node
-          nextState.doc.descendants((node, pos) => {
-            if (requiredAddAttr(node)) {
-              if (DOC_NAME == node.type.name) {
-                tr = tr.step(new SetDocAttrStep(ATTR_NAME, guidGenerator()));
-              } else {
-                const attrs = node.attrs;
-                tr.setNodeMarkup(pos, undefined, {
-                  ...attrs,
-                  [ATTR_NAME]: guidGenerator()
-                });
-                modified = true;
-              }
-            }
-          });
-        }
-        trackDeletedObjectId(prevState, nextState);
-        return modified ? tr : null;
-      },
-    });
-  }
+		super({
+			key: new PluginKey('ObjectIdPlugin'),
+			state: {
+				init(config, state) {},
+				apply(tr, set) {}
+			},
+			props: {
+				handleDOMEvents: {
+					keydown(view, event) {
+						const charCode = event.key;
+						console.log(charCode);
+					}
+				},
+				nodeViews: []
+			},
+			appendTransaction: (transactions, prevState, nextState) => {
+				let tr = null;
+				if (isDocChanged(transactions)) {
+					tr = assingIDsForMissing(prevState, nextState);
+					trackDeletedObjectId(prevState, nextState);
+				}
+				return tr;
+			},
+		});
+	}
 
 }
-// returns new guid
+
 function guidGenerator() {
-  return uuid();
+	// returns new guid
+	// prefix with "new:" to identify the newly created ids.
+	return "new:" + uuid();
+}
+
+function isDocChanged(transactions) {
+	return (transactions.some((transaction) => transaction.docChanged));
+}
+
+function assingIDsForMissing(prevState, nextState) {
+	let tr = nextState.tr;
+	let modified = false;
+	// Adds a unique id to the document
+	if (requiredAddAttr(nextState.doc)) {
+		tr = tr.step(new SetDocAttrStep(ATTR_NAME, guidGenerator()));
+		modified = true;
+		//docModified = true;
+	}
+
+	// Adds a unique id to a node
+	nextState.doc.descendants((node, pos) => {
+		if (requiredAddAttr(node)) {
+			const attrs = node.attrs;
+			tr.setNodeMarkup(pos, undefined, {
+				...attrs,
+				[ATTR_NAME]: guidGenerator()
+			});
+			modified = true;
+		}
+	});
+
+	return modified ? tr : null;
+}
+
+function nodeAssignment(state) {
+	const nodesById = {};
+	state.doc.descendants((node) => {
+		if (isTargetNodeAllowed(node)) {
+			if (node.attrs.objectId) {
+				nodesById[node.attrs.objectId] = node;
+			}
+		}
+	});
+
+	return nodesById;
 }
 
 // track the deleted nodes
 function trackDeletedObjectId(prevState, nextState) {
-  if (prevState.doc !== nextState.doc) {
-    const prevNodesById = {};
-    const deletedIds = new Set();
+	if (prevState.doc !== nextState.doc) {
+		let prevNodesById = {};
+		let nextNodesById = {};
+		const deletedIds = new Set();
 
-    prevState.doc.descendants((node) => {
-      if (isTargetNodeAllowed(node)) {
-        if (node.attrs.objectId) {
-          prevNodesById[node.attrs.objectId] = node;
-        }
-      }
-    })
-    const nextNodesById = {}
-    nextState.doc.descendants((node) => {
-      if (isTargetNodeAllowed(node)) {
-        if (node.attrs.objectId) {
-          nextNodesById[node.attrs.objectId] = node;
-        }
-      }
-    })
+		prevNodesById = nodeAssignment(prevState);
+		nextNodesById = nodeAssignment(nextState);
 
-    for (const [id, node] of Object.entries(prevNodesById)) {
-      if (nextNodesById[id] === undefined) {
-        deletedIds.add(id);
-      }
-    }
-    console.log({
-      deletedIds
-    });
-  }
+		for (const [id, node] of Object.entries(prevNodesById)) {
+			if (nextNodesById[id] === undefined) {
+				deletedIds.add(id);
+			}
+		}
+		console.log({
+			deletedIds
+		});
+	}
 }
