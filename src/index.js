5b3a4a73c64f891acbac2fc0de5092c375c092ba
Nicholas Fiorello <nfiorello@modusoperandi.com@MO11493.modusoperandi.com>
2020-11-18 12:50:37 -0500

    delete the comment that should have been deleted along with the code

--- src/index.js
+++ src/index.js
@@ -8,6 +8,3 @@ export { default as uuid } from './ui/uuid';
 export { default as Licit } from './client/Licit.js';
 export { ImageLike, EditorRuntime } from './Types';
 export { GET, POST } from './client/http';
-
-// Export the plugin so that consumers of the library have the access required
-// to actually create one.
