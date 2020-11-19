36c40bbfb23c98b9c4e08227b55d230b0d1e935a
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2020-11-19 19:27:18 +0530

    1. Renamed to licit-doc-attrs-step so that all of our modules that are for the licit ecosystem are visually related. 2. Exporting the class as a named export rather than the default because it forces the name to be used consistently.

--- helpers/doc-attrs-step/src/index.js
+++ helpers/doc-attrs-step/src/index.js
@@ -1,5 +1,6 @@
 // [FS] IRAD-??? 2020-10-19
 // Plugin to handle automatic assign unique id to the block nodes.
 
-export { default as SetDocAttrStep } from './SetDocAttrStep';
+// Exporting the class as a named export rather than the default because it forces the name to be used consistently.
+export { SetDocAttrStep } from './SetDocAttrStep';
 
