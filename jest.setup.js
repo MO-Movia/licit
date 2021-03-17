b8b8af3872b12cc2243978f7b27f4635ef11c481
Nicholas Fiorello <nfiorello@modusoperandi.com>
2021-03-17 15:21:54 -0400

    limit tests to src folder

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,5 @@
 // setup for jest because jquery is needed by node-mathquill
 window.jQuery = require('jquery');
-
-
+// needed to mock this due to execute during loading
 document.execCommand =
   document.execCommand || function execCommandMock() {};
