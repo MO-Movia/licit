d6f1320a2391768822bc0aba93bd9109b08c6ed7
seybi.ea <seybi.ea@firmusoft.co.in>
2021-05-27 10:36:33 +0530

    Added new attribute in TexthighlightMark to handle citation node position.

--- src/TextHighlightMarkSpec.js
+++ src/TextHighlightMarkSpec.js
@@ -10,6 +10,7 @@ const TextHighlightMarkSpec: MarkSpec = {
   attrs: {
     highlightColor: {default: ''},
     hasCitation: {default: false},
+    pos: {default: null},
   },
   inline: true,
   group: 'inline',
@@ -17,28 +18,31 @@ const TextHighlightMarkSpec: MarkSpec = {
     {
       tag: 'span[style*=background-color]',
       getAttrs: (dom: HTMLElement) => {
-        const {backgroundColor, zIndex} = dom.style;
+        const {backgroundColor, zIndex, opacity} = dom.style;
         const color = toCSSColor(backgroundColor);
         return {
           highlightColor: isTransparent(color) ? '' : color,
           hasCitation: zIndex === '1' ? true : false,
+          pos: opacity,
         };
       },
     },
   ],
 
   toDOM(node: Node) {
-    const {highlightColor, hasCitation} = node.attrs;
+    const {highlightColor, hasCitation, pos} = node.attrs;
     let style = '';
     const empty = '';
     // [FS] IRAD-1361 2021-05-18
     // Mange Copy paste citation applied text
     // added new attribute hasCitation - This should be in plugin but it will not create any issue.
+    // added new attribute pos - to identify the citation node pos.
+    // TODO: Need to mange the normal highlight
     if (highlightColor && !hasCitation) {
       style += `background-color: ${highlightColor};`;
     }
     if (hasCitation) {
-      style += `background-color: ${empty};z-index: 1`;
+      style += `background-color: ${empty};z-index: 1;opacity :${pos}`;
     }
     return ['span', {style}, 0];
   },
