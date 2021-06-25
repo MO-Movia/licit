2142e4e3e499c69c35ab18995b7b80e71e547331
arathy.gopal <seybi.ea@firmusoft.co.in>
2021-06-25 15:20:11 +0530

    1. FIX: Indenting not remove when clear style is applied

--- src/CustomStyleCommand.js
+++ src/CustomStyleCommand.js
@@ -235,6 +235,9 @@ class CustomStyleCommand extends UICommand {
     let tr = this.clearCustomStyles(state.tr.setSelection(selection), state);
 
     hasMismatchHeirarchy(state, tr, node, startPos, endPos);
+    // [FS] IRAD-1480 2021-06-25
+    // Indenting not remove when clear style is applied
+    newattrs = Object.assign({}, node ? node.attrs : {});
     newattrs['styleName'] = RESERVED_STYLE_NONE;
     newattrs['id'] = '';
     tr = tr.setNodeMarkup(startPos, undefined, newattrs);
