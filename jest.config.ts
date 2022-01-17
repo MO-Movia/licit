38a74946cf4a99f219fc54c1c7c6c2d940c59149
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2022-01-17 07:50:44 +0530

    - FIX: Configure jest to transform some dependencies, so that import statement succeeds.

--- jest.config.ts
+++ jest.config.ts
@@ -181,14 +181,14 @@ export default {
 
   // A map from regular expressions to paths to transformers
   transform: {
-      "^.+\\.tsx?$": "babel-jest"
-    },
+   "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
+  transformIgnorePatterns: [
+   "node_modules/(?!(y-protocols|lib0|@hocuspocus)/).+\\.js$"
   //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
