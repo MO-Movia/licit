e56e57b886e4a2e8fed272f2ffeb7b45f30aed59
seybi.ea <seybi.ea@firmusoft.co.in>
2022-01-18 13:01:47 +0530

    Added typesript to run test

--- src/components/licit/extensions/toolBar/toolBar.test.tsx
+++ src/components/licit/extensions/toolBar/toolBar.test.tsx
@@ -2,10 +2,12 @@ import { Extension } from '@tiptap/core';
 import { Plugin, PluginKey, EditorState, Transaction } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { toggleMark, wrapIn } from 'prosemirror-commands';
+import { v4 as uuidv4 } from 'uuid';
 
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Toolbar from './toolBar';
+import { isAnyArrayBuffer } from 'util/types';
 
 jest.mock('@tiptap/core');
 jest.mock('prosemirror-state');
@@ -15,10 +17,14 @@ jest.mock('./menu.css');
 jest.mock('react');
 jest.mock('react-dom');
 
-describe('Toolbar', () => {  
-    describe('Toolbar type', () => {
-      it('should match the Extension', () => {
-        expect(Toolbar.type).toBeDefined();
-      });
+describe('Toolbar', () => {
+  // let tb = Toolbar.({
+  //   name: 'LiciT-TBar-' + uuidv4(),
+  // });
+
+  describe('Toolbar type', () => {
+    it('should match the Extension', () => {
+      expect(Toolbar).toBeInstanceOf(Extension);
     });
   });
+});
