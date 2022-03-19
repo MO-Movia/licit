46baa6f8f86298870e12e0e081f4eb3b0d2f1235
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2022-03-19 12:26:07 +0530

    - Removed the temp fix for IRAD-1738, as it is now handled in the plugin.

--- src/ui/handleEditorKeyDown.js
+++ src/ui/handleEditorKeyDown.js
@@ -29,12 +29,6 @@ export default function handleEditorKeyDown(
   if (from === to - 1) {
     const node = tr.doc.nodeAt(from);
     if (node.isAtom && !node.isText && node.isLeaf) {
-      // [FS] IRAD-1738 2022-03-18
-      // This is for handling "Copy Special" when image(s) is selected.
-      // TODO: Need to move this to rich-copy-embed-images plugin. Have to find a way for that.
-      if (event.ctrlKey && event.altKey && 'C' === event.key.toUpperCase()) {
-        return false;
-      }
       // An atomic node (e.g. Image) is selected.
       // Only whitelisted keyCode should be allowed, which prevents user
       // from typing letter after the atomic node selected.
