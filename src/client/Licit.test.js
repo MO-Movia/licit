2f54db564aafb736276a5003554bc15bb56221e0
arathy.gopal <arathy.gopal@firmusoft.co.in>
2022-06-20 19:31:37 +0530

    Upgraded the react version

--- src/client/Licit.test.js
+++ src/client/Licit.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Licit from './Licit';
 import RichTextEditor from '../ui/RichTextEditor';
 import { noop } from '@modusoperandi/licit-ui-commands';
