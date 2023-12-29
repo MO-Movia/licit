cc664c3d9173de22e2b2dcd4657c415b9ad91c02
Drishya Das <drishya.das@modusoperandi.com>
2023-12-29 19:22:45 +0530

    Blade Editor strict rule fix

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+export { EditorState } from "prosemirror-state";
+export { default as isEditorStateEmpty } from "./isEditorStateEmpty";
+export { default as uuid } from "./ui/uuid";
+export { default as Licit, DataType } from "./client/Licit.js";
+export { ImageLike, EditorRuntime } from "./Types";
+export { GET, POST, DELETE, PATCH } from "./client/http";
