612d1a842e031360a812a749e956a603e925735f
rohan.john <rohan.john@firmusoft.co.in>
2024-06-21 15:17:57 +0530

    Added HTMLStyles type for Style

--- licit/client/CustomLicitRuntime.js
+++ licit/client/CustomLicitRuntime.js
@@ -18,6 +18,35 @@ const TYPE_JSON = 'application/json; charset=utf-8';
 
 // const GLOSSARY_URI = 'http://greathints.com:3003';
 
+type HTMLStyles = {
+  align?: string; // Text align
+  boldNumbering?: boolean; // true= Bold the Numbering part
+  boldPartial?: boolean; // true = Bold first word
+  boldSentence?: boolean; // true = Bold first sentence
+  fontName?: string; // Font Name
+  fontSize?: string; // Font size
+  strong?: boolean; // Bold
+  em?: boolean; // Italic
+  underline?: boolean; // Text Underline
+  color?: string; // Text colour
+  textHighlight?: string; // Text highlight
+  hasNumbering?: boolean; // true= The style has numbering
+  hasBullet?: boolean; // true= The style has bullet
+  paragraphSpacingAfter?: string; // Spacing after a Paragraph
+  paragraphSpacingBefore?: string; // Spacing before a Paragraph
+  styleLevel?: number; // Numbering heirachy level
+  bulletLevel?: string | boolean; // Numbering heirachy level
+  lineHeight?: string; // Line spacing
+  isLevelbased?: boolean; // true= Text indent will be based on Level
+  indent?: string; // Text indent
+  nextLineStyleName?: string;
+  toc?: boolean;
+  isHidden?: boolean;
+  strike?: string;
+  isList?: boolean;
+  prefixValue?: string;
+};
+
 type Style = {
   /**
    * Name of the style. Case insensitive value must be unique.
