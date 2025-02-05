024546c48122a2e7256f89188e537e76aba7cd57
rohan <rohan.john@modusoperandi.com>
2025-02-05 20:23:02 +0530

    Editor hang issue while applying OL from menu bar

--- src/OrderedListNodeSpec.js
+++ src/OrderedListNodeSpec.js
@@ -21,8 +21,8 @@ const OrderedListNodeSpec: NodeSpec = {
     listStyleType: { default: null },
     name: { default: null },
     start: { default: 1 },
-    type: { default: 'decimal' },
-    styleName: { default: RESERVED_STYLE_NONE },
+    type: { default: 'decimal' }
+   
   },
   group: 'block',
   content: LIST_ITEM + '+',
