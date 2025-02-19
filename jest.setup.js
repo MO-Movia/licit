584cfb80d1e92d0f8a7cd13d4b0ea1ff61ffd867
Ryan Culp <rculp@modusoperandi.com>
2025-02-18 23:04:35 -0500

    removed dev/removed dependencies from the peer/dep list.

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,2 @@
-// setup for jest because jquery is needed by node-mathquill
-// window.jQuery = require('jquery');
-// import jQuery from 'jquery';
 // needed to mock this due to execute during loading
-document.execCommand =
-  document.execCommand || function execCommandMock() {};
+document.execCommand = document.execCommand || function execCommandMock() {};
