04ee88f876623feb4f86527eaec5dda570605363
rohan.john <rohan.john@firmusoft.co.in>
2025-05-12 14:52:53 +0530

    Resolved PR comments.

--- src/MarksClearCommand.js
+++ src/MarksClearCommand.js
@@ -1,12 +1,11 @@
 // @flow
 
-import { EditorState, AllSelection, TextSelection } from 'prosemirror-state';
+import { EditorState } from 'prosemirror-state';
 import { Transform } from 'prosemirror-transform';
 import { EditorView } from 'prosemirror-view';
 import * as React from 'react';
 import { clearMarks, clearHeading } from '@modusoperandi/licit-ui-commands';
 import { UICommand } from '@modusoperandi/licit-doc-attrs-step';
-import { CellSelection } from 'prosemirror-tables';
 
 class MarksClearCommand extends UICommand {
   isActive = (state: EditorState): boolean => {
@@ -16,8 +15,7 @@ class MarksClearCommand extends UICommand {
   isEnabled = (state: EditorState) => {
     const { selection } = state;
     return (
-      !selection.empty &&
-      (selection instanceof TextSelection || selection instanceof AllSelection || selection instanceof CellSelection)
+      !selection.empty
     );
   };
 
